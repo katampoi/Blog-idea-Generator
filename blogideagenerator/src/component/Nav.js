aa0023d21e45344782ff031f06e673a660b3651c
katampoi <simonkatampoi@student.moringaschool.com>
2022-07-26 14:33:53 +0300

    added content on nav component

--- blogideagenerator/src/component/Nav.js
+++ blogideagenerator/src/component/Nav.js
@@ -3,6 +3,12 @@ import React from 'react'
 function Nav() {
   return (
     <div className='nav'>
+        <div className='navtest'>
+        <h2> Ready to serve when your brain is out of service</h2>
+        <h1>IT'S FREE!</h1> 
+        </div>
+        
+           
       
     </div>
   )
