0fb967b5ecbcfc8f7ac9c54d4244cbbcc64b031e
katampoi <simonkatampoi@student.moringaschool.com>
2022-08-02 09:05:17 +0300

    added description

--- blogideagenerator/src/component/Form.js
+++ blogideagenerator/src/component/Form.js
@@ -1,28 +1,21 @@
 import React, { useState,useEffect } from 'react'
-
 function Form() {
     // const [values, setValues] = useState({
     //    blogName:"",
     //    description:"",
     // });
-
     // const handleBlogNameInputChange = (event) => {
     //     setValues({...values, blogName: event.target.value})
     // }
-
     // const handledescriptionInputChange = (event) => {
     //     setValues({...values, description: event.target.value})
     // }
     const [ posts,setPosts] = useState([]);
-       const [searchValue, setsearchValue] = useState([]);
+       const [searchValue, setSearchValue] = useState("");
        useEffect(() => {
        fetch("http://localhost:8000/posts")
          .then((r) => r.json())
-         .then((data) => {
-           data.array.forEach(element => {
-            console.log(element)
-           });;
-         });
+         .then((data) => setPosts(data));
      }, []);
      console.log(posts)
      const postsList = posts.filter((val)=>{
@@ -32,41 +25,23 @@ function Form() {
             return val;
         }
       }).map((doc)=>(
-        <h3 key={doc._id}>{doc.title}</h3>
-        
+        <h3 key={doc.id}>{doc.title} {doc.idea}</h3>
       ))
     // const test= posts.map((result)=> (
     //     <h3 key={result.id}>
     //       {result.title}
     //     </h3>
     //  ))
-    
-
-
   return (
     <div className='form'>
-        <form className='formcontent'>
             <input
-            onChange={(event)=>setsearchValue(event.target.value)}
-            // value={values.blogName}
+            type="text"
+            onChange={(event)=>setSearchValue(event.target.value)}
             className='formField'
             placeholder='Blog Name/Topic'
-            name='blogName'
-            /> <br/>
-
-            {/* <input
-            onChange={handledescriptionInputChange}
-            value={values.description}
-            className='formField'
-            placeholder='Description of your Blog'
-            name='description'
-            /> */}
-        </form>
+            />
         {postsList}
-
-      
     </div>
   )
 }
-
 export default Form
